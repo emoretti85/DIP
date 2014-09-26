e22fad5e08a47ba15e713b8f5bff1f2af931e35c
emoretti85 <ettoremoretti27@gmail.com>
2014-09-26 17:23:31 +0200

    Create EmergencyPage.php

---
+++ EmergencyPage.php
@@ -0,0 +1,4 @@
+<?php
+
+// Of course, I leave to you the decision on how to use this page. Is a php page, come on! use your imagination :)
+echo "Page of emergency to be presented to users in case of detection defacing";
